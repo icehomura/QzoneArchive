c2645af31f59374242e58f3df4d641c147db262f
冰晓焰 <icehomura@qq.com>
2026-08-12 06:28:00 +0800

    ci: build NixOS package from Nix derivation

--- nix/qzonearchive.nix
+++ nix/qzonearchive.nix
@@ -1,56 +1,57 @@
 { pkgs }:
 let
-  nodejs = pkgs.nodejs_22;
   lib = pkgs.lib;
   stdenv = pkgs.stdenv;
+  rustPlatform = pkgs.rustPlatform;
+  cargo-tauri = pkgs.cargo-tauri;
+  nodejs = pkgs.nodejs_22;
 in
-stdenv.mkDerivation {
+rustPlatform.buildRustPackage {
   pname = "qzonearchive";
   version = "1.0.3";
 
   src = lib.cleanSource ../.;
 
-  nativeBuildInputs = with pkgs; [
+  cargoRoot = "src-tauri";
+  buildAndTestSubdir = "src-tauri";
+
+  cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+
+  npmDeps = pkgs.fetchNpmDeps {
+    name = "qzonearchive-1.0.3-npm-deps";
+    inherit (lib.cleanSource ../.) src;
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  nativeBuildInputs = [
     nodejs
+    pkgs.npmHooks.npmConfigHook
     pkg-config
     cmake
-    gcc
-    gnumake
-    rustc
-    cargo
-    perl
-    wrapGAppsHook3
+    cargo-tauri.hook
+  ] ++ lib.optionals stdenv.hostPlatform.isLinux [
+    pkgs.wrapGAppsHook4
   ];
 
-  buildInputs = with pkgs; [
-    glib
-    gtk3
-    webkitgtk_4_1
-    libsoup_3
-    openssl
-    patchelf
-    sqlite
-    xdotool
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
+  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
+    pkgs.glib-networking
+    pkgs.openssl
+    pkgs.sqlite
+    pkgs.xdotool
+    pkgs.webkitgtk_4_1
+    pkgs.glib
+    pkgs.gtk3
+    pkgs.libsoup_3
+    pkgs.patchelf
+    pkgs.gst_all_1.gst-plugins-base
+    pkgs.gst_all_1.gst-plugins-good
+    pkgs.gst_all_1.gst-plugins-bad
   ];
 
-  configurePhase = ''
-    npm ci
-  '';
+  doCheck = false;
 
-  buildPhase = ''
-    npm run build
-    npx tauri build --no-bundle --ci
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    install -Dm755 src-tauri/target/release/qzonearchive "$out/bin/qzonearchive"
-
-    install -Dm644 src-tauri/icons/icon.png "$out/share/icons/hicolor/512x512/apps/qzonearchive.png"
+  postInstall = ''
+    install -Dm644 "$src/src-tauri/icons/icon.png" "$out/share/icons/hicolor/512x512/apps/qzonearchive.png"
 
     mkdir -p "$out/share/applications"
     cat > "$out/share/applications/qzonearchive.desktop" <<EOF
@@ -62,7 +63,5 @@ Exec=$out/bin/qzonearchive
 Icon=qzonearchive
 Categories=Utility;
 EOF
-
-    runHook postInstall
   '';
 }
